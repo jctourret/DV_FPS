abfe9523541d03e491398f34e30a698029c3ef5b
Juan Cruz Tourret <jctourret@gmail.com>
2020-05-03 15:31:59 -0300

    Importacion de Assets, Agregados jugador, pistola y retina de disparo

---
+++ DV - FPS/Assets/Scripts/GhostScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GhostScript : MonoBehaviour
+{
+    int GhostHitPoints = 10;
+    void Update()
+    {
+        if (GhostHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void DeductPoints(int damage)
+    {
+        GhostHitPoints -= damage;
+    }
+}
---
+++ DV - FPS/Assets/Scripts/PistolDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PistolDamage : MonoBehaviour
+{
+    [SerializeField] int damage = 5;
+    [SerializeField] float targetDistance;
+    [SerializeField] float AllowedRange = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.yellow);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit shot;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+            {
+                targetDistance = shot.distance;
+                if (targetDistance < AllowedRange)
+                {
+                    shot.transform.SendMessage("DeductPoints", damage);
+                }
+
+            }
+        }        
+    }
+}
