39a4ad3beb83ecb0f56b2f156163551a0e2bb6ec
Juan Cruz Tourret <jctourret@gmail.com>
2020-05-04 08:16:27 -0300

    Agregadas animaciones de retina, display the municion y cajas de municion

---
+++ DV - FPS/Assets/AmmoPickUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GlobalAmmo.currentAmmo += 10;
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ DV - FPS/Assets/Scripts/GlobalAmmo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAmmo : MonoBehaviour
+{
+    static public int currentAmmo;
+    public int internalAmmo;
+    public GameObject ammoDisplay;
+    void Update()
+    {
+        internalAmmo = currentAmmo;
+        ammoDisplay.GetComponent<Text>().text = "" + internalAmmo;
+    }
+}
